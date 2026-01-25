d12423027c3a89c800c4a63f755d4c09c3c7ace0
leventev <levente.vamos@protonmail.com>
2026-01-25 23:30:39 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [zig];
+      packages = with pkgs; [qemu]; 
+    };
+  };
+}

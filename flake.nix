bde0b91e7b82556cf7eef6e8ca064af7d9234a53
leventev <levente.vamos@protonmail.com>
2026-01-29 17:05:25 +0100

    flake.nix: add binutils

--- flake.nix
+++ flake.nix
@@ -9,11 +9,12 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+    pkgsCross = pkgs.pkgsCross.riscv64-embedded;
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [zig];
-      packages = with pkgs; [qemu]; 
+      packages = [pkgs.qemu pkgsCross.binutils]; 
     };
   };
 }
